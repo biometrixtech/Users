fef00ce08707ddc119812e5704d4f9ccce9a6db1
Bob Davis <bob@bookly.co>
2018-04-06 21:59:17 -0600

    Modified lambda_task_root_key manipulation to work locally when not defined

--- apigateway/apigateway.py
+++ apigateway/apigateway.py
@@ -5,8 +5,9 @@ import traceback
 
 
 # Break out of Lambda's X-Ray sandbox so we can define our own segments and attach metadata, annotations, etc, to them
-lambda_task_root_key = os.getenv('LAMBDA_TASK_ROOT')
-del os.environ['LAMBDA_TASK_ROOT']
+lambda_task_root_key = os.getenv('LAMBDA_TASK_ROOT', ".")
+if lambda_task_root_key != ".":
+    del os.environ['LAMBDA_TASK_ROOT']
 from aws_xray_sdk.core import patch_all, xray_recorder
 from aws_xray_sdk.core.models.trace_header import TraceHeader
 patch_all()
