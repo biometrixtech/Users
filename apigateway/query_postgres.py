c7a6e0b4bcb0fede931f3c88ca61a10fbda07955
Stephen Poole <stephen@melon.software>
2018-09-25 11:11:54 +0000

    Don't need to use the full ARN here

--- apigateway/query_postgres.py
+++ apigateway/query_postgres.py
@@ -6,9 +6,9 @@ import os
 
 @xray_recorder.capture('apigateway.query_postgres')
 def query_postgres(query, parameters):
-    lambda_client = boto3.client('lambda', region_name=os.environ['AWS_REGION'])
+    lambda_client = boto3.client('lambda')
     res = json.loads(lambda_client.invoke(
-        FunctionName='arn:aws:lambda:{AWS_REGION}:{AWS_ACCOUNT_ID}:function:infrastructure-{ENVIRONMENT}-querypostgres'.format(**os.environ),
+        FunctionName='infrastructure-{ENVIRONMENT}-querypostgres'.format(**os.environ),
         Payload=json.dumps({
             "Queries": [{"Query": query, "Parameters": parameters}],
             "Config": {"ENVIRONMENT": os.environ['ENVIRONMENT']}
