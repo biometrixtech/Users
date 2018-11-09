27c9b4450f1a5777538f1d8edf5d8e33f5715f3c
Stephen Poole <stephen@melon.software>
2018-11-09 15:10:34 +0000

    Fix properly updating accounts when adding users, and vice versa

--- apigateway/models/user_data.py
+++ apigateway/models/user_data.py
@@ -1,5 +1,10 @@
+from boto3.dynamodb.conditions import Attr
+
 from fathomapi.api.config import Config
 from fathomapi.models.dynamodb_entity import DynamodbEntity
+from fathomapi.utils.exceptions import NoSuchEntityException
+
+import models.account
 
 
 class UserData(DynamodbEntity):
@@ -9,5 +14,43 @@ class UserData(DynamodbEntity):
         super().__init__({'id': user_id})
 
     @property
-    def user_id(self):
+    def id(self):
         return self.primary_key['id']
+
+    def add_account(self, account_id):
+        """
+        Link the user to an account
+        :param str account_id:
+        """
+        if not self.exists():
+            raise NoSuchEntityException(f'No user with id {self.id}')
+
+        if account_id in self.get()['account_ids']:
+            return
+
+        upsert = self.DynamodbUpdate()
+        upsert.add('account_ids', {account_id, '_empty'})
+        self._update_dynamodb(upsert, Attr('id').exists())
+
+        self._attributes.setdefault('account_ids', []).append(account_id)
+
+        models.account.Account(account_id).add_user(self.id)
+
+    def remove_account(self, account_id):
+        """
+        Unlink the user from an account
+        :param str account_id:
+        """
+        if not self.exists():
+            raise NoSuchEntityException(f'No user with id {self.id}')
+
+        if account_id not in (self.get()['account_ids'] or []):
+            return
+
+        upsert = self.DynamodbUpdate()
+        upsert.delete('account_ids', {account_id})
+        self._update_dynamodb(upsert, Attr('id').exists())
+
+        self._attributes['account_ids'].remove(account_id)
+
+        models.account.Account(account_id).add_user(self.id)
